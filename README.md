afd757d8bd95f6757b648f8a1669e2690821fee7
Furgus4 <ryantomich80@gmail.com>
2025-09-07 20:08:21 -0400

    uncomplete test dropdown

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-A Chrome Extension that improves YouTube Search functionality.
+This is a work in progress.
+
+I don't have a finished prototype yet.
+
+## Current UI iteration
+<img width="3399" height="1029" alt="attempt-3" src="https://github.com/user-attachments/assets/a460d617-867b-4319-8e45-5c6a78f7590c" />
+
+## UI in context
+<img width="3319" height="912" alt="attempt-3 in context" src="https://github.com/user-attachments/assets/6a9e8250-99ee-4d3c-b64c-b905295dcdbb" />
--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 // Wait for the bar with chips to appear before starting
-const chipBarPath = "ytd-app > div#content > ytd-page-manager > ytd-search > div#container > div#header"
+const chipBarPath = "ytd-app > div#content > ytd-page-manager > ytd-search > div#container > div#header > ytd-search-header-renderer";
 
 const observerTarget = document.querySelector("ytd-app");
 const config = { childList: true, subtree: true }
@@ -7,20 +7,27 @@ const config = { childList: true, subtree: true }
 const observationCallback = (mutationList, observer) => {
   if (document.querySelector(chipBarPath) !== null) {
     observer.disconnect();
-    init();
+    main();
   }
 }
 
 const observer = new MutationObserver(observationCallback);
 observer.observe(observerTarget, config);
 
+function main() {
+  let toolbar = document.querySelector(chipBarPath);
+  for (const child of toolbar.children) {
+    child.style.display = "none";
+  }
 
-function init() {
-  const oldBar = document.querySelector(chipBarPath);
-  let newBar = document.createElement('div');
-  newBar.className = "toolbar";
+  const durationAndViews = document.createElement("div");
+  durationAndViews.append("Duration & Views");
+  durationAndViews.id = "duration-and-views";
 
-  const parent = oldBar.parentNode;
-  parent.appendChild(newBar);
-  parent.removeChild(oldBar);
+  durationAndViews.addEventListener("mouseup", (e) => {
+    console.log("open dropdown");
+  });
+
+  // needs dropdown icon
+  toolbar.appendChild(durationAndViews);
 }
