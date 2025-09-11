a2875eff83300ce303f887c3e713a6a68bc794ac
Furgus4 <Ryantomich80@gmail.com>
2025-09-10 22:29:55 -0400

    hiding functionality for duration (not continuous)

--- content.js
+++ content.js
@@ -7,7 +7,6 @@
 // I should also probably put all the initial observers in a
 // separate file from the main function
 
-
 const chipBarPath = "ytd-app > div#content > ytd-page-manager > ytd-search > div#container > div#header > ytd-search-header-renderer";
 const chipBar = document.querySelector("ytd-app");
 const config = { childList: true, subtree: true }
@@ -20,15 +19,15 @@ const observationCallback = (mutationList, observer) => {
 const chipObserver = new MutationObserver(observationCallback);
 chipObserver.observe(chipBar, config);
 
-
 function main() {
   let toolbar = document.querySelector(chipBarPath);
   for (const child of toolbar.children) {
     child.style.display = "none";
   }
 
+  // temp prototype ui
   const durationAndViews = document.createElement("div");
-  durationAndViews.append("Duration & Views"); // needs interactable dropdown icon
+  durationAndViews.append("Duration & Views");
   durationAndViews.id = "duration-and-views";
   toolbar.appendChild(durationAndViews);
 
@@ -46,27 +45,24 @@ function main() {
       davDropdown.style.display = "flex";
     } else if (davDropdown.style.display === "flex") {
       davDropdown.style.display = "none";
-      // definitely call this on blur of the input fields instead of here
+      // consider calling this on blur of the input fields instead of here
+      // I need to set up an observer that detects when videos load and call this there as well
       davFunctionality();
     }
   });
 
+  // All dropdowns and their functionality should get their own file
   const davFunctionality = () => {
     const min = document.getElementById("duration-min").value;
     const max = document.getElementById("duration-max").value;
 
-    // I could potentially use an api to get video length here,
-    // but that seems convoluted so for now I'll just scrape the html
-
-    // I'll eventually need an observer to filter videos as they load
+    console.log("min: " + min);
+    console.log("max: " + max);
 
     const videoDisplayArea = document.querySelector("ytd-search.style-scope.ytd-page-manager > div#container > ytd-two-column-search-results-renderer > div#primary > ytd-section-list-renderer > div#contents");
 
     for (const child of videoDisplayArea.children) {
-      console.log(child.tagName);
       if (child.tagName === "YTD-ITEM-SECTION-RENDERER") {
-        // check duration of each ytd-video-renderer in div#contents
-        console.log("here are the videos (potentially)");
         const videoList = child.querySelector("div#contents");
         for (const potentialVideo of videoList.children) {
           // ytd-video-renderer is a video
@@ -74,20 +70,24 @@ function main() {
           // ytd-ad-slot-renderer is probably an ad, (test without adblocker)
           // grid-shelf-view-model has shorts in it I think
 
-          // right now just do videos
           if (potentialVideo.tagName === "YTD-VIDEO-RENDERER") {
             // right now this also gets SHORTS as a duration,
             // ill probably need to find some other way to get the duration consistently
             // I don't really want to make api calls though
+
             const duration = potentialVideo.querySelector("div.yt-badge-shape__text").textContent;
-            console.log(duration);
-            // convert duration to minutes with math and string stuff
-            // if it's not SHORTS check if it fits in the min-max variables
+            if (duration === "SHORTS") {
+              console.log("You obviously need to find a better way of doing this");
+              potentialVideo.hidden = true // gets rid of shorts for now
+            }
+
+            let minutes = +duration.split(":")[0];
+            if (minutes < min || minutes > max) { // not entirely accurate because of unaccounted seconds
+              potentialVideo.hidden = true;
+            }
           }
         }
       }
     }
   }
 }
-
-// All dropdowns and their functionality will get their own file
