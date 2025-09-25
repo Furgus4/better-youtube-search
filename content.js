a89174cb545d2555960a28cf786b8f7688cbba98
Furgus4 <ryantomich80@gmail.com>
2025-09-25 09:31:57 -0400

    added basic data extraction with injected script

--- content.js
+++ content.js
@@ -1,3 +1,12 @@
+
+window.addEventListener("yt-navigate-finish", checkURL);
+
+function checkURL() {
+  if (document.URL.startsWith("https://www.youtube.com/results")) {
+    injectScript('video-data-scraper.js');
+  }
+}
+
 function injectScript (src) {
     const s = document.createElement('script');
     s.src = chrome.runtime.getURL(src);
@@ -5,4 +14,9 @@ function injectScript (src) {
     (document.head || document.documentElement).append(s);
 }
 
-injectScript('injected-script.js');
+// I don't like this method
+// not only is it constantly updating, I'm storing the data twice
+// I might have to implement some sort of handshake method where I request the data as I use it
+window.addEventListener("dataSent", e => {
+  console.log(e.detail);
+});
