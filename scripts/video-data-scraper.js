ce6fb66fa8cc1dc1e177b264c1c232395ee435a6
Furgus4 <ryantomich80@gmail.com>
2025-10-22 17:33:33 -0400

    videos are now only observed once instead of hundreds of times, over and over again, as anything loads

--- scripts/video-data-scraper.js
+++ scripts/video-data-scraper.js
@@ -5,103 +5,125 @@ if (ytInitialData.contents.twoColumnSearchResultsRenderer === undefined) {
 
 const data = new Map();
 
-function addData(type, title, duration, views, uploadYear, watched) {
-  const newData = { type: type, title: title, duration: duration, views: views, uploadYear: uploadYear, watched: watched, hidden: false }
-  data.set(title, newData);
-  checkVideo(data.get(title));
-}
+let elementDisplayArea = document.querySelector("ytd-search.style-scope.ytd-page-manager > div#container > ytd-two-column-search-results-renderer > div#primary > ytd-section-list-renderer > div#contents");
+let globalI = 0;
 
-const videoDataObserver = new MutationObserver(getDataFromSearchResults);
-let videoDisplayArea = document.querySelector("ytd-search.style-scope.ytd-page-manager > div#container > ytd-two-column-search-results-renderer > div#primary > ytd-section-list-renderer > div#contents");
-videoDataObserver.observe(videoDisplayArea, { attributes: false, childList: true, subtree: true });
+// this initial call is here because observers don't account for elements (videos) already loaded
+extractDataFromGroup(elementDisplayArea);
 
-function getDataFromSearchResults() {
-  const elementGroupings = videoDisplayArea.children;
-  for (let i = 0; i < elementGroupings.length; i++) {
-    if (elementGroupings[i].tagName !== "YTD-ITEM-SECTION-RENDERER") continue;
+const observeNewGroups = new MutationObserver(extractDataFromNewGroups);
+observeNewGroups.observe(elementDisplayArea, {attributes: false, childList: true, subtree: false });
 
-    const elements = elementGroupings[i].querySelector("div#contents").children;
-
-    for (let j = 0; j < elements.length; j++) {
-      const rawData = ytInitialData
-          .contents.twoColumnSearchResultsRenderer
-          .primaryContents.sectionListRenderer
-          .contents[i].itemSectionRenderer
-          .contents[j];
-
-      // video or short or livestream
-      if (elements[j].tagName === "YTD-VIDEO-RENDERER") {
-        const title = rawData.videoRenderer.title.runs[0].text;
-        if (data.has(title)) continue;
-
-        const duration = getSecondsFromDuration(rawData.videoRenderer.lengthText.simpleText);
-        const views = +rawData.videoRenderer.viewCountText.simpleText.split(" ")[0].split(",").join("");
-        const uploadYear = getActualUploadYear(rawData.videoRenderer.publishedTimeText.simpleText);
-        const watched = rawData.videoRenderer.isWatched || false;
-
-        let type = duration > 60*3 ? "video" : "short";
-        if (rawData.videoRenderer.publishedTimeText.simpleText.split(" ")[0] === "Streamed") {
-          type = "live";
-        }
-
-        // I did this so that I could easily hide the video,
-        // but I may just find the video in the hiding function using the title and querySelector instead
-        elements[j].id = title;
-        addData(type, title, duration, views, uploadYear, watched);
-
-      // playlist
-      } else if (elements[j].tagName === "YT-LOCKUP-VIEW-MODEL") {
-        const title = rawData.lockupViewModel.metadata.lockupMetadataViewModel.title.content;
-        if (data.has(title)) continue;
-
-        const duration = undefined;
-        const views = undefined;
-        const type = "playlist";
-        const uploadYear = undefined;
-        const watched = undefined;
-        const subType = undefined;
-
-        elements[j].id = title;
-        addData(type, title, duration, views, uploadYear, watched, subType);
-
-      // ad? have yet to test without ad blocker
-      } else if (elements[j].tagName === "YTD-AD-SLOT-RENDERER") {
-        elements[j].hidden = true;
-
-      // grid of shorts
-      } else if (elements[j].tagName === "GRID-SHELF-VIEW-MODEL") {
-        elements[j].hidden = true; // hide for now because I'm lazy
-
-        // I need to loop through the shorts in the rows and treat them as separate videos
-        // ytGridShelfViewModelGridShelfRow
-        // I may want to abstract the previous video/short section of this whole thing and use it here too
-
-      } else if (elements[j].tagName === "YTD-SEARCH-PYV-RENDERER") {
-        // idek
-
-      // people also watched
-      } else if (elements[j].tagName === "YTD-SHELF-RENDERER") {
-        elements[j].hidden = true; // hiding for now because i'm lazy
-
-      // channel
-      } else if (elements[j].tagName === "YTD-CHANNEL-RENDERER") {
-        const title = rawData.channelRenderer.title.simpleText;
-        if (data.has(title)) continue;
-
-        const duration = undefined;
-        const views = undefined;
-        const type = "channel";
-        const uploadYear = undefined;
-        const watched = undefined;
-        const subType = undefined;
-
-        elements[j].id = title;
-        addData(type, title, duration, views, uploadYear, watched, subType);
+function extractDataFromNewGroups(mutationList) {
+  for (const mutation of mutationList) {
+    for (const node of mutation.addedNodes) {
+      if (node.querySelector("#contents") !== null) {
+        extractDataFromGroup(node);
       }
     }
   }
 }
 
+function extractDataFromGroup(group) {
+  const elementGroup = group.querySelector("#contents");
+  let i = globalI;
+  globalI += 1;
+  let j = 0;
+
+  // observers don't account for these initially loaded elements
+  for (const element of elementGroup.children) {
+    extractElementData(i, j, element.tagName);
+    j++;
+  }
+  
+  observeNewElementsInGroup(elementGroup, i, j);
+}
+
+function observeNewElementsInGroup(group, i, j) {
+  const extractDataFromElementsInGroup = (mutationList) => {
+    for (const mutation of mutationList) {
+      for (const element of mutation.addedNodes) {
+        extractElementData(i, j, element.tagName);
+        j++;
+      }
+    }
+  }
+
+  const observer = new MutationObserver(extractDataFromElementsInGroup);
+  observer.observe(group, { childList: true });
+}
+
+function extractElementData(i, j, tagName) {
+  const rawData = ytInitialData
+    .contents.twoColumnSearchResultsRenderer
+    .primaryContents.sectionListRenderer
+    .contents[i].itemSectionRenderer
+    .contents[j];
+
+  // video, short, or livestream
+  if (tagName === "YTD-VIDEO-RENDERER") {
+    const title = rawData.videoRenderer.title.runs[0].text;
+
+    const duration = getSecondsFromDuration(rawData.videoRenderer.lengthText.simpleText);
+    const views = +rawData.videoRenderer.viewCountText.simpleText.split(" ")[0].split(",").join("");
+    const uploadYear = getActualUploadYear(rawData.videoRenderer.publishedTimeText.simpleText);
+    const watched = rawData.videoRenderer.isWatched || false;
+
+    let type = duration > 60*3 ? "video" : "short";
+    if (rawData.videoRenderer.publishedTimeText.simpleText.split(" ")[0] === "Streamed") {
+      type = "live";
+    }
+
+    data.set(title, { type: type, title: title, duration: duration, views: views, uploadYear: uploadYear, watched: watched, hidden: false });
+    //checkVideo(data.get(title));
+  }
+
+  // playlist
+  else if (tagName === "YT-LOCKUP-VIEW-MODEL") {
+    const title = rawData.lockupViewModel.metadata.lockupMetadataViewModel.title.content;
+
+    const duration = undefined;
+    const views = undefined;
+    const type = "playlist";
+    const uploadYear = undefined;
+    const watched = undefined;
+
+    data.set(title, { type: type, title: title, duration: duration, views: views, uploadYear: uploadYear, watched: watched, hidden: false });
+    //checkVideo(data.get(title));
+  }
+
+  // have yet to test without ad blocker
+  else if (tagName === "YTD-AD-SLOT-RENDERER") {}
+
+  // grid of shorts
+  else if (tagName === "GRID-SHELF-VIEW-MODEL") {
+    // I need to loop through the shorts in the rows and treat them as separate videos
+    // ytGridShelfViewModelGridShelfRow
+    // I may want to abstract the previous video/short section of this whole thing and use it here too
+  }
+
+  // idek
+  else if (tagName === "YTD-SEARCH-PYV-RENDERER") {}
+
+  // people also watched
+  else if (tagName === "YTD-SHELF-RENDERER") {}
+
+  // channel
+  else if (tagName === "YTD-CHANNEL-RENDERER") {
+    const title = rawData.channelRenderer.title.simpleText;
+
+    const duration = undefined;
+    const views = undefined;
+    const type = "channel";
+    const uploadYear = undefined;
+    const watched = undefined;
+
+    data.set(title, { type: type, title: title, duration: duration, views: views, uploadYear: uploadYear, watched: watched, hidden: false });
+    //checkVideo(data.get(title));
+  }
+}
+
+// consider a helpers.js to store stuff like this
 function getSecondsFromDuration(d) {
   let result = 0;
   let arr = d.split(":").reverse();
